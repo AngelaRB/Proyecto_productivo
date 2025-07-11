82389cca85c66e7da2f6ff9ca54d9bb227694f0e
David Guerrero <mandere4841t@gmail.com>
2025-07-11 16:54:54 -0500

    Primer commit

---
+++ js/pomodoro.js
@@ -0,0 +1,101 @@
+window.onload = () => {
+
+    let breaktime;
+    let worktime; 
+    let restTime;
+    let timesCompleted;
+    let cyclesGoal;
+    let cyclesCompleted = 0;
+
+    function pomodoroController() {
+        if (isRestTime()) {
+            cyclesCompleted++;
+            if (!goalReached()) {
+                currentTime = restTime;
+                timer();
+                timesCompleted = 0;
+            } else {
+                console.log("¡Se acabó el pomodoro!");
+            }
+            return;
+        }
+
+        if (timesCompleted % 2 == 0) {
+            currentTime = worktime;
+            timesCompleted++;
+            timer();
+            console.log("¡Hora de trabajar! Ciclo: " + timesCompleted);
+        } else {
+            currentTime = breaktime;
+            timesCompleted++;
+            timer();
+            console.log("Hora de descansar 😌 Ciclo: " + timesCompleted);
+        }
+    }
+
+    function isRestTime() {
+        return timesCompleted == 7;
+    }
+
+    function goalReached() {
+        return cyclesGoal == cyclesCompleted;
+    }
+
+    let clock = document.getElementById("clock");
+    let cyclesInput = document.getElementById("cycles-input");
+    let startButton = document.getElementById("start-button");
+    let workTimeInput = document.getElementById("work-time");
+    let breakTimeInput = document.getElementById("break-time");
+    let restTimeInput = document.getElementById("rest-time");
+
+    startButton.onclick = () => {
+        populateVariable();
+        startPomodoro();
+    };
+
+    function startPomodoro() {
+        console.log("Empezó el pomodoro");
+        pomodoroController();
+    }
+
+    function populateVariable() {
+        console.log("Variables cargadas");
+        worktime = parseInt(workTimeInput.value);
+        breaktime = parseInt(breakTimeInput.value);
+        restTime = parseInt(restTimeInput.value);
+        cyclesGoal = parseInt(cyclesInput.value);
+        timesCompleted = 0;
+    }
+
+    let clockminutes;
+    let clockseconds;
+
+    function updateClock() {
+        clockminutes = formatnumbers(currentTime);
+        clockseconds = formatnumbers(seconds);
+        clock.innerHTML = clockminutes + ":" + clockseconds;
+    }
+
+    function formatnumbers(time) {
+        return time < 10 ? "0" + time : time;
+    }
+
+    let currentTime = 1;
+    let seconds = 0;
+
+    function timer() {
+        if (currentTime > 0 || seconds > 0) {
+            if (seconds == 0) {
+                seconds = 59;
+                currentTime--;
+            } else {
+                seconds--;
+            }
+            updateClock();
+            setTimeout(timer, 1000);
+        } else {
+            pomodoroController();
+            console.log("El temporizador terminó");
+        }
+    }
+};
